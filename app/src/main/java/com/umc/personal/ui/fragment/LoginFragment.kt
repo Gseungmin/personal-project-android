21bb8fbc7f865fdf93866c3f63ceddff527eb66f
Ji Seungmin <cswcsm02@gmail.com>
2023-03-29 17:16:08 +0900

    Design: Login Fragment 디자인 구현

---
+++ app/src/main/java/com/umc/personal/ui/fragment/LoginFragment.kt
@@ -0,0 +1,57 @@
+package com.umc.personal.ui.fragment
+
+import android.os.Bundle
+import android.util.Patterns
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.umc.personal.databinding.FragmentLoginBinding
+import java.util.regex.Pattern
+
+class LoginFragment : Fragment() {
+
+    private var _binding : FragmentLoginBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View? {
+        _binding = FragmentLoginBinding.inflate(inflater, container, false)
+        val view = binding.root
+
+        //이메일 유효성 검사
+        validate_email()
+
+        return view
+    }
+
+    /**시작시 로그인 상태 확인 메소드*/
+    override fun onStart() {
+        super.onStart()
+    }
+
+    private fun validate_email() {
+        binding.emailLoginButton.setOnClickListener {
+            val pattern: Pattern = Patterns.EMAIL_ADDRESS
+
+            val email = binding.email.text
+
+            if (pattern.matcher(email).matches()) {
+            }
+        }
+    }
+
+    /**
+     * viewBinding이 더이상 필요 없을 경우 null 처리 필요
+     */
+    override fun onDestroy() {
+        _binding = null
+        super.onDestroy()
+    }
+}
